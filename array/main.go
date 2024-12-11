d043342d1b00f86a9884db7f4b20cf3d7b3b23ca
Shub12388y <spl.sp999@gmail.com>
2024-12-12 00:05:57 +0530

    added arrays

---
+++ array/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// create an array
+	var names [4]int
+	fmt.Println(names)
+
+
+	nums :=[4]int{1,2,3,4}
+	fmt.Println(nums)
+
+
+	// 2d array
+	numed :=[2][2]int{{1,2},{3,4}}
+	fmt.Println(numed)
+}
