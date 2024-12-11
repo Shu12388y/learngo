0e15c1abf63e1bf1383264c15551793759ab3e15
Shub12388y <spl.sp999@gmail.com>
2024-12-11 23:38:03 +0530

    added variables

---
+++ variables/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	// define variables
+	var name string = "Golang";
+	fmt.Println(name);
+
+
+	// infer
+	var named = "Golang";
+	fmt.Println(named);
+
+
+	// shorthand syntax
+	names := "Golang";
+	fmt.Println(names);
+
+
+
+
+}
