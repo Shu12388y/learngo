a82c3e8738359781f455953c2deb32f942073bcf
Shub12388y <spl.sp999@gmail.com>
2024-12-11 23:33:20 +0530

    added values

---
+++ simple_values/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// Integer
+	fmt.Println(1);
+
+	// String
+	fmt.Println("Shubham");
+
+	// float 
+	fmt.Println(10.5)
+
+	// boolean
+	fmt.Println(true)
+
+}
